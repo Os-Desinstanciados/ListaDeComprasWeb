ec429c4bb7610c9221e7c231c19d0de56380f7cd
Alexandre Rocha <juny14_15@hotmail.com>
2026-06-04 16:23:57 -0300

    Adicionado Refatoração com View Model

--- ListaDeCompras.WebApp/Program.cs
+++ ListaDeCompras.WebApp/Program.cs
@@ -1,17 +1,17 @@
 // APS.NET Core
-
 // Builder de um server web
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // MVC
 builder.Services.AddControllersWithViews();
 
+// Criação de Instancia do servidor Web
 WebApplication app = builder.Build();
 
-// Middlewares
+// Middlewares - Funções que executam a cada chamada
+app.UseStaticFiles();
 app.UseRouting();
 app.MapDefaultControllerRoute();
 
 // Iniciar o looping da aplicação
 app.Run();
-
