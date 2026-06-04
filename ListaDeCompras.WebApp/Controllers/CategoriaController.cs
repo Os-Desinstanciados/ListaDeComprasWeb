397d2d022ac48838abf01475c359aefacdfc076d
Alexandre Rocha <juny14_15@hotmail.com>
2026-06-04 13:37:48 -0300

    Adicionado Editar Categoria

--- ListaDeCompras.WebApp/Controllers/CategoriaController.cs
+++ ListaDeCompras.WebApp/Controllers/CategoriaController.cs
@@ -19,6 +19,7 @@ public class CategoriaController : Controller
     }
 
     // GET: CategoriaController
+    [HttpGet]
     public ActionResult Listar()
     {
         List<Categoria> categorias = repositorioCategoria.SelecionarTodos();
@@ -27,8 +28,42 @@ public class CategoriaController : Controller
         return View(categorias);
     }
 
+    [HttpGet]
     public ActionResult Cadastrar()
     {
         return View();
+    }
+
+    [HttpPost]
+    public ActionResult Cadastrar(string nome, string cor)
+    {
+        Categoria novaCategoria = new Categoria(nome, cor);
+
+        repositorioCategoria.Cadastrar(novaCategoria);
+        
+        string listarStr = nameof(Listar);
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Editar(string id)
+    {
+        Categoria? categoria = repositorioCategoria.SelecionarPorId(id);
+
+        if (categoria == null)
+            return RedirectToAction(nameof(Listar));
+
+        return View(categoria);
+    }
+
+    [HttpPost]
+    public ActionResult Editar(string id, string nome, string cor)
+    {
+        Categoria categoriaAtualizada = new Categoria(nome, cor);
+
+        repositorioCategoria.Editar(id, categoriaAtualizada);
+
+        return RedirectToAction(nameof(Listar));
+
     }
 }
