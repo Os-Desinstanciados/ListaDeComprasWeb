7ec554a7f1e4138c996322ee584610261f2a0fe3
Alexandre Rocha <juny14_15@hotmail.com>
2026-06-05 14:29:37 -0300

    Adicionado CRUD e ViewModels em ModuloProduto

--- ListaDeCompras.WebApp/Models/ProdutoViewModels.cs
+++ ListaDeCompras.WebApp/Models/ProdutoViewModels.cs
@@ -1,2 +1,32 @@
 namespace ListaDeCompras.WebApp.Models;
 
+public record ListarProdutoViewModel(
+    string Id,
+    string Nome,
+    string Categoria,
+    string UnidadeMedida,
+    decimal PrecoAproximado
+);
+
+public record CadastrarProdutoViewModel(
+    string Nome,
+    string CategoriaId,
+    string UnidadeMedida,
+    decimal PrecoAproximado
+);
+
+public record EditarProdutoViewModel(
+    string Id,
+    string Nome,
+    string CategoriaId,
+    string UnidadeMedida,
+    decimal PrecoAproximado
+);
+
+public record ExcluirProdutoViewModel(
+    string Id,
+    string Nome,
+    string Categoria,
+    string UnidadeMedida,
+    decimal PrecoAproximado
+);
