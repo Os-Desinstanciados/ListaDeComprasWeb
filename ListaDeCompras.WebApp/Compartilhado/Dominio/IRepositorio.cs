550539dc26f58c8e02e3e8a4c14e5c491d1366c2
Alexandre Rocha <juny14_15@hotmail.com>
2026-06-08 16:31:18 -0300

    Arrumado o Bug do Repositorio

--- ListaDeCompras.WebApp/Compartilhado/Dominio/IRepositorio.cs
+++ ListaDeCompras.WebApp/Compartilhado/Dominio/IRepositorio.cs
@@ -3,8 +3,9 @@ namespace ListaDeCompras.WebApp.Compartilhado.Dominio;
 public interface IRepositorio<T> where T : EntidadeBase<T>
 {
     void Cadastrar(T entidade);
-    bool Editar(string idSelecionado, T entidadeAtualizada);
-    bool Excluir(T registro);
-    T? SelecionarPorId(string idSelecionado);
+    bool Editar(Guid idSelecionado, T entidadeAtualizada);
+    bool Excluir(Guid idSelecionado);
+    T? SelecionarPorId(Guid idSelecionado);
     List<T> SelecionarTodos();
+    List<T> Filtrar(Predicate<T> filtro);
 }
